2f41e274d90ac622594a883dd5550ed0de23efed
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-12-26 10:37:27 -0300

    Mudança visual

--- scripts/script.js
+++ scripts/script.js
@@ -43,15 +43,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		
 		const li = document.createElement('li');
+		
+		const editButtonIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 96 960 960" width="1.5em"><path fill="currentColor" d="M199 857q-6-6-6-14t6-14l534-534q6-6 14-6t14 6q6 6 6 14t-6 14L227 857q-6 6-14 6t-14-6Z"/></svg>';
+		const deleteButtonIcon = '<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em"><path fill="currentColor" d="M13.05 42q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.5H9.5q-.65 0-1.075-.425Q8 9.65 8 9q0-.65.425-1.075Q8.85 7.5 9.5 7.5h7.9q0-.65.425-1.075Q18.25 6 18.9 6h10.2q.65 0 1.075.425.425.425.425 1.075h7.9q.65 0 1.075.425Q40 8.35 40 9q0 .65-.425 1.075-.425.425-1.075.425h-.55V39q0 1.2-.9 2.1-.9.9-2.1.9Zm5.3-8.8q0 .65.425 1.075.425.425 1.075.425.65 0 1.075-.425.425-.425.425-1.075V16.25q0-.65-.425-1.075-.425-.425-1.075-.425-.65 0-1.075.425-.425.425-.425 1.075Zm8.3 0q0 .65.425 1.075.425.425 1.075.425.65 0 1.075-.425.425-.425.425-1.075V16.25q0-.65-.425-1.075-.425-.425-1.075-.425-.65 0-1.075.425-.425.425-.425 1.075Z"/></svg>'
+		
 		li.innerHTML = `
 		<input type="checkbox" class="checkbox" ${completed ? 'checked' : ''} />
 		<span>${taskText}</span>
 		<div class="task-buttons">
 			<button class="edit-button">
-				<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 96 960 960" width="1.5em"><path fill="currentColor" d="M199 857q-6-6-6-14t6-14l534-534q6-6 14-6t14 6q6 6 6 14t-6 14L227 857q-6 6-14 6t-14-6Z"/></svg>
+				${editButtonIcon}
 			</button>
 			<button class="delete-button">
-				<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" height="1.5em" width="1.5em"><path fill="currentColor" d="M13.05 42q-1.2 0-2.1-.9-.9-.9-.9-2.1V10.5H9.5q-.65 0-1.075-.425Q8 9.65 8 9q0-.65.425-1.075Q8.85 7.5 9.5 7.5h7.9q0-.65.425-1.075Q18.25 6 18.9 6h10.2q.65 0 1.075.425.425.425.425 1.075h7.9q.65 0 1.075.425Q40 8.35 40 9q0 .65-.425 1.075-.425.425-1.075.425h-.55V39q0 1.2-.9 2.1-.9.9-2.1.9Zm5.3-8.8q0 .65.425 1.075.425.425 1.075.425.65 0 1.075-.425.425-.425.425-1.075V16.25q0-.65-.425-1.075-.425-.425-1.075-.425-.65 0-1.075.425-.425.425-.425 1.075Zm8.3 0q0 .65.425 1.075.425.425 1.075.425.65 0 1.075-.425.425-.425.425-1.075V16.25q0-.65-.425-1.075-.425-.425-1.075-.425-.65 0-1.075.425-.425.425-.425 1.075Z"/></svg>
+				${deleteButtonIcon}
 			</button>
 		</div>
 		`;
